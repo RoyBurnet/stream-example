0b7c21d2ba5274d532f12f3e2ef589561d53578e
Randy Burnet <rrw.burnet@gmail.com>
2019-12-27 23:38:17 +0100

    create streamlist

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -22,10 +22,12 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -40,14 +42,14 @@ export const fetchStream = id => async dispatch => {
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
-export const deleteStream = id => async dispatch => {
-  await streams.delete(`/streams/${id}`);
-
-  dispatch({ type: DELETE_STREAM, payload: id });
-};
-
 export const editStream = id => async dispatch => {
   const response = await streams.put(`/streams/${id}`);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
